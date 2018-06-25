7ea7ce0b2adefe5f4a82a39e21506e9dbef8b7ef
Tatyana Scotce <tscotce@gmail.com>
2018-06-25 20:07:54 +0000

    automatically backed up by learn

--- Rakefile
+++ Rakefile
@@ -8,3 +8,18 @@ require 'sinatra/activerecord/rake'
 task :console do
   Pry.start
 end
+
+# namespace :db do
+#   desc 'migrate changes to your database'
+#   task :create_migration => :environment do
+#     Movie.create_table
+#   end
+#   task :environment do
+#     require_relative './config/environment'
+#   end
+  
+#   desc 'seed the database with some dummy data'
+#   task :seed do 
+#     require_relative './db/seeds.rb'
+#   end
+# end
--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,36 +19,36 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = m)
   Movie.create do |m|
-    __
+    m.new
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.first(id)
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by attribute
 end
 
 def can_find_using_where_clause_and_be_sorted
