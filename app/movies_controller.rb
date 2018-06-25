4f63f3cf3248018775d36ebffd4e5dd6078cc0ba
Tatyana Scotce <tscotce@gmail.com>
2018-06-25 20:11:33 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new
+  movie = self.new
   movie.title = "This is a title."
   movie
 end
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new
+  movie = self.new
 end
 
 def can_be_created_in_a_block(args = m)
