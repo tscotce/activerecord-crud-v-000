d913de74cdc07e7dd4ed0bfd6467326eeef1ae74
Tatyana Scotce <tscotce@gmail.com>
2018-06-25 20:12:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,15 +33,15 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last
+  self.last
 end
 
 def can_get_size_of_the_database
-  Movie.size
+  self.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.first(id)
+  self.first(:id)
 end
 
 def can_find_by_multiple_attributes
